d9b74f76c0aa852cb8a2d93e36564bf7327fd4ec
softwaregravy <426690+softwaregravy@users.noreply.github.com>
2020-04-26 11:05:26 -0400

    add administrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :animals
+
+      root to: "animals#index"
+    end
   resources :animals
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
